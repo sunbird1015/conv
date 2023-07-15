075530dd635a985cc1ab9ebba1b47087c2bab21f
sdd <dd.sh@163.com>
2023-07-15 20:00:35 +0800

    conv.String support map/struct conv with json marshal; change conv.conv to conv.Conv

--- conv.go
+++ conv.go
@@ -1,11 +1,12 @@
 package conv
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 )
 
-func conv(v interface{}, o interface{}) error {
+func Conv(v interface{}, o interface{}) error {
 	config := &DecoderConfig{
 		Result:           o,
 		WeaklyTypedInput: true,
@@ -18,7 +19,7 @@ func conv(v interface{}, o interface{}) error {
 	}
 	err = decoder.decode("", v, reflect.ValueOf(decoder.config.Result).Elem())
 	if err != nil {
-		fmt.Println(err.Error())
+		//fmt.Println(err.Error())
 		return err
 	}
 	return nil
@@ -26,196 +27,200 @@ func conv(v interface{}, o interface{}) error {
 
 func Int(v interface{}) int {
 	var output int
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Ints(v interface{}) []int {
 	var output []int
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int8(v interface{}) int8 {
 	var output int8
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int8s(v interface{}) []int8 {
 	var output []int8
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int16(v interface{}) int16 {
 	var output int16
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int16s(v interface{}) []int16 {
 	var output []int16
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int32(v interface{}) int32 {
 	var output int32
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int32s(v interface{}) []int32 {
 	var output []int32
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int64(v interface{}) int64 {
 	var output int64
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Int64s(v interface{}) []int64 {
 	var output []int64
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint(v interface{}) uint {
 	var output uint
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uints(v interface{}) []uint {
 	var output []uint
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint8(v interface{}) uint8 {
 	var output uint8
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint8s(v interface{}) []uint8 {
 	var output []uint8
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint16(v interface{}) uint16 {
 	var output uint16
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint16s(v interface{}) []uint16 {
 	var output []uint16
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint32(v interface{}) uint32 {
 	var output uint32
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint32s(v interface{}) []uint32 {
 	var output []uint32
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint64(v interface{}) uint64 {
 	var output uint64
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Uint64s(v interface{}) []uint64 {
 	var output []uint64
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Bool(v interface{}) bool {
 	var output bool
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Bools(v interface{}) []bool {
 	var output []bool
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func String(v interface{}) string {
 	var output string
-	conv(v, &output)
+	if err := Conv(v, &output); err != nil {
+		if ret, err := json.Marshal(v); err == nil {
+			return string(ret)
+		}
+	}
 	return output
 }
 
 func Strings(v interface{}) []string {
 	var output []string
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Map(v interface{}) map[string]interface{} {
 	var output map[string]interface{}
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func MapStrStr(v interface{}) map[string]string {
 	var output map[string]string
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Maps(v interface{}) []map[string]interface{} {
 	var output []map[string]interface{}
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Interfaces(v interface{}) []interface{} {
 	var output []interface{}
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Struct(v interface{}, o interface{}) {
-	conv(v, o)
+	Conv(v, o)
 }
 
 func Float32(v interface{}) float32 {
 	var output float32
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Float32s(v interface{}) []float32 {
 	var output []float32
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Float64(v interface{}) float64 {
 	var output float64
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
 
 func Float64s(v interface{}) []float64 {
 	var output []float64
-	conv(v, &output)
+	Conv(v, &output)
 	return output
 }
